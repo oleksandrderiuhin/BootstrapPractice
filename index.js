7a58aba03eda2bfe15c7693a7a96f480509b2211
oleksandr_deriuhin <oleksandrderiuhin97@gmail.com>
2024-11-11 21:27:41 +0200

    Start work
    With first template

---
+++ index.js
@@ -0,0 +1,16 @@
+// const toastTrigger = document.getElementById('liveToastBtn')
+// const toastLiveExample = document.getElementById('liveToast')
+
+// if (toastTrigger) {
+//   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+//   toastTrigger.addEventListener('click', () => {
+//     toastBootstrap.show()
+//   })
+// }
+
+document.getElementById('liveToastBtn')?.addEventListener('click', () => {
+    const toastLiveExample = document.getElementById('liveToast');
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+    toastBootstrap.show();
+  });
+  
